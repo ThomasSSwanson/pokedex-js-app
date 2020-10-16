956b89e12a468858bd75ed8bc09fd679009f7f27
Biggleby <lastedguy@gmail.com>
2020-10-15 20:22:50 -0400

    added simple js

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let myName = 'Bob';
+document.write(myName + "<br>");
+myName = 'John Doe';
+document.write(myName);
+
+alert('Hello world');
+
+let favoriteFood = 'Pizza';
+document.write(favoriteFood);
