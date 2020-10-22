cb4fc228af82ff05741055edf294cb1155f79952
Biggleby <lastedguy@gmail.com>
2020-10-21 20:13:21 -0400

    added background and styled list

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,47 @@
 let pokemonList = [
-  {
-    name: "Charmander",
-    type: "fire",
-    height: "2\'"
+  {name: 'Bulbasaur',
+  height: 0.7,
+  type: ['grass', ' poison']
   },
-
-  {
-    name: "Squirtle",
-    type: "water",
-    height: "1\'8\""
-  }
-
-  {
-    name: "Bulbasaur",
-    type: "grass",
-    height: "2\'4\""
-  }
-
+  {name: 'Ivysaur',
+  height: 1,
+  type: ['grass', ' poison']
+  },
+  {name: 'Venusaur',
+  height: 2,
+  type: ['grass', ' poison']
+  },
+  {name: 'Charmander',
+  height: 0.6,
+  type: ['fire']
+  },
+  {name: 'Charmeleon',
+  height: 1.1,
+  type: ['fire']
+  },
+  {name: 'Charizard',
+  height: 1.7,
+  type: ['fire', ' flying']
+  },
+  {name: 'Squirtle',
+  height: 0.5,
+  type: ['water']
+  },
+  {name: 'Wartortle',
+  height: 1,
+  type: ['water']
+  },
+  {name: 'Blastoise',
+  height: 1.6,
+  type: ['water']
+}
 ]
 
-document.write(pokemonList[1].height)
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write("<h2>" + pokemonList[i].name + "</h2><br>" + "<h3>type: </h3>" + pokemonList[i].type + "<br>");
+    if (pokemonList[i].height >= 1.5) {
+      document.write("<h3>height: </h3>" + pokemonList[i].height + " Wow that is big!! <br><br>");
+    }  else if (pokemonList[i].height < 1.5) {
+      document.write("<h3>height: </h3>" + pokemonList[i].height + "<br><br>")
+    }
+}
