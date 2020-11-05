3dd5d99dea44114da682cf7cf97c54d6061189bb
Biggleby <lastedguy@gmail.com>
2020-11-05 17:32:39 -0500

    implemented pokemon list API

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,12 @@
-// SETTING INITIAL LIST TO PULL FROM WILL LATER USE API
-
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {name: 'Bulbasaur',
-    height: 0.7,
-    type: ['grass', ' poison']
-    },
-    {name: 'Ivysaur',
-    height: 1,
-    type: ['grass', ' poison']
-    },
-    {name: 'Venusaur',
-    height: 2,
-    type: ['grass', ' poison']
-    },
-    {name: 'Charmander',
-    height: 0.6,
-    type: ['fire']
-    },
-    {name: 'Charmeleon',
-    height: 1.1,
-    type: ['fire']
-    },
-    {name: 'Charizard',
-    height: 1.7,
-    type: ['fire', ' flying']
-    },
-    {name: 'Squirtle',
-    height: 0.5,
-    type: ['water']
-    },
-    {name: 'Wartortle',
-    height: 1,
-    type: ['water']
-    },
-    {name: 'Blastoise',
-    height: 1.6,
-    type: ['water']
-    }
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+
 
   function add(pokemon) {
-    if (typeof(pokemon) === "object" && Object.keys(pokemon).length === 3){
+    if (typeof(pokemon) === "object"){
       pokemonList.push(pokemon);
-    }
-    
+    } 
   }
 
   function getAll() {
@@ -52,12 +14,14 @@ let pokemonRepository = (function () {
   }
 
   function showDetails(pokemon) {
-    console.log(pokemon);
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
   }
 
   function buttonListener (button, poke) {
     button.addEventListener('click', function (event) {
-      showDetails(poke.name);
+      showDetails(poke);
     });
   }
 
@@ -72,20 +36,48 @@ let pokemonRepository = (function () {
     buttonListener(button, pokemon);
   }
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Now we add the details to the item
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
   return {
     add: add,
     getAll: getAll,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 })();
 
-// FOREACH LOOP TO DOCUMENT.WRITE TO INDEX.HTML DEPENDING ON POKEMON HEIGHT AND TYPE 
-function displayPokemon() {
-  let pokemonList = pokemonRepository.getAll();
-  pokemonList.forEach(function(pokemon) {
+pokemonRepository.loadList().then(function() {
+  // Now the data is loaded!
+  pokemonRepository.getAll().forEach(function(pokemon){
     pokemonRepository.addListItem(pokemon);
-})};
-
-
-pokemonRepository.add({name: 'Pidgey', height: 0.4, type: ['normal', ' flying']});
-displayPokemon();
+  });
+});
